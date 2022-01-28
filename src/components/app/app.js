30401c19dbc3915ae76c136c0b45f32d44171b90
Nodir <wedeveloper93@gmail.com>
2022-01-28 15:47:45 +0500

    Search and Filter

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,4 @@
-import { Component,  } from 'react/cjs/react.production.min';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { Component } from 'react';
 
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
@@ -8,210 +6,111 @@ import AppFilter from '../app-filter/app-filter';
 import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
-
 import './app.css';
 
-
-// class WhoAmI extends Component{
-//   constructor(props){
-//     super(props);
-//     this.state = {
-//       years: 27,
-//       text: '+++',
-//       position: ''
-//     }
-//   }
-//   nextYear = () => {
-//     console.log('+++');
-//     this.setState({
-//       years: this.state.years + 1
-//     })
-//   }
-//   commitInputChanges = (e) =>{
-//     this.setState({
-//       position: e.target.value
-//     })
-//   }
-//   render() {
-//     const {name, surname, link} = this.props;
-//     const {position, years} = this.state;
-//     return(
-//       <div>
-//         <button onClick={this.nextYear}>{this.state.text}</button>
-//         <h1>{name}, surname - {surname}, age - {years}, position - {position}</h1>
-//         <a href={link}>My site</a>
-//         <form>
-//           <span>Vvedite doljnost: </span>
-//           <input type="text" onChange={this.commitInputChanges}/>
-//         </form>
-//       </div>
-//     )
-//   }
-// }
-
 class App extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      data: [
-        {name:'John C.', salary:800, increase: true, id: 1},
-        {name:'Alex M', salary:3000, increase: false, id: 2},
-        {name:'Karl G.', salary:5000, increase: false, id: 3}
-      ],
-      term: ''
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {name: 'John C.', salary: 800, increase: false, rise: true, id: 1},
+                {name: 'Alex M.', salary: 3000, increase: true, rise: false, id: 2},
+                {name: 'Carl W.', salary: 5000, increase: false, rise: false, id: 3}
+            ],
+            term: '', 
+            filter: 'all'
+        }
+        this.maxId = 4;
     }
-    this.maxId = 4
-  }
-  deleteItem = (id) =>{
-    this.setState(({data})=>{
-      const index = data.findIndex(elem=>elem.id === id);
-      // const before = data.slice(0, index);
-      // const after = data.slice(index+1);
-      // const newArr = [...before, ...after];
-      return {
-        // data: newArr
-        data: data.filter(item=>item.id !==id)
-      }
-    })
-  }
-  addItem = (name,salary)=>{
-    const newItem = {
-      name,
-      salary,
-      increase: false,
-      id: this.maxId++
-    }
-    this.setState(({data})=>{
-      const newArr = [...data, newItem];
-      return{
-        data: newArr
-      }
-    })
-  }
-  onToggleProp = (id, prop) => {
-    this.setState(({data}) => ({
-        data: data.map(item => {
-            if (item.id === id) {
-                return {...item, [prop]: !item[prop]}
+
+    deleteItem = (id) => {
+        this.setState(({data}) => {
+            return {
+                data: data.filter(item => item.id !== id)
             }
-            return item;
         })
-    }))
-  }
-  searchEmp = (items, term) => {
-    if (term.length === 0) {
-        return items;
     }
 
-    return items.filter(item => {
-        return item.name.indexOf(term) > -1
-    })
-  }
+    // Да, пока могут добавляться пустые пользователи. Мы это еще исправим
+    addItem = (name, salary) => {
+        const newItem = {
+            name, 
+            salary,
+            increase: false,
+            rise: false,
+            id: this.maxId++
+        }
+        this.setState(({data}) => {
+            const newArr = [...data, newItem];
+            return {
+                data: newArr
+            }
+        });
+    }
 
-  onUpdateSearch = (term) =>{
-    this.setState({term})
-  }
+    onToggleProp = (id, prop) => {
+        this.setState(({data}) => ({
+            data: data.map(item => {
+                if (item.id === id) {
+                    return {...item, [prop]: !item[prop]}
+                }
+                return item;
+            })
+        }))
+    }
 
-  render() {
-    const {data, term} = this.state;
-    const employees = this.state.data.length;
-    const increased = this.state.data.filter(item => item.increase).length;
-    const visibleData = this.searchEmp(data, term);
-    return (
-      <div className="app">
-        <AppInfo employees={employees} increased={increased}/>
-        <div className="search-panel">
-            <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
-            <AppFilter/>
-        </div>
-        
-        <EmployeesList 
-          data={visibleData}
-          onDelete={this.deleteItem}
-          onToggleProp={this.onToggleProp}
-        />
-        <EmployeesAddForm onAdd={this.addItem}/>
-      </div>
-    );
-  }
+    searchEmp = (items, term) => {
+        if (term.length === 0) {
+            return items;
+        }
+
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1
+        })
+    }
+
+    onUpdateSearch = (term) => {
+        this.setState({term});
+    }
+
+    filterPost = (items, filter) => {
+        switch (filter) {
+            case 'rise':
+                return items.filter(item => item.rise);
+            case 'moreThen1000':
+                return items.filter(item => item.salary > 1000);
+            default:
+                return items
+        }
+    }
+
+    onFilterSelect = (filter) => {
+        this.setState({filter});
+    }
+
+    render() {
+        const {data, term, filter} = this.state;
+        const employees = this.state.data.length;
+        const increased = this.state.data.filter(item => item.increase).length;
+        const visibleData = this.filterPost(this.searchEmp(data, term), filter);
+
+        return (
+            <div className="app">
+                <AppInfo employees={employees} increased={increased}/>
+    
+                <div className="search-panel">
+                    <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
+                    <AppFilter filter={filter} onFilterSelect={this.onFilterSelect}/>
+                </div>
+                
+                <EmployeesList 
+                    data={visibleData}
+                    onDelete={this.deleteItem}
+                    onToggleProp={this.onToggleProp}/>
+                <EmployeesAddForm onAdd={this.addItem}/>
+            </div>
+        );
+    }
 }
 
-
 export default App;
-
-// const data = [
-//   {name:'John C.', salary:800, increase: true, id: 1},
-//   {name:'Alex M', salary:3000, increase: false, id: 2},
-//   {name:'Karl G.', salary:5000, increase: false, id: 3}
-// ]
-
-/* <WhoAmI name="saloh" surname="malik" link="https://www.olx.uz/"/> */ 
-
-/* <AppInfo />
-
-        <div className="search-panel">
-            <SearchPanel/>
-            <AppFilter/>
-        </div>
-        
-        <EmployeesList data={data}/>
-        <EmployeesAddForm/> */
-
-        // class WhoAmI extends Component{
-        //   constructor(props){
-        //     super(props);
-        //     this.state = {
-        //       counter: 27,
-        //       text: '+++'
-        //     }
-        //   }
-        //   nextYear = () => {
-        //     console.log('+++');
-        //     this.setState({
-        //       counter: this.state.counter + 1
-        //     })
-        //   }
-        //   render() {
-        //     const {name, surname, link} = this.props;
-        //     return(
-        //       <div>
-        //         <button onClick={this.nextYear}>{this.state.text}</button>
-        //         <h1>{name}, surname - {surname}, age - {this.state.counter}</h1>
-        //         <a link={link}>My Profile</a>
-        //       </div>
-        //     )
-        //   }
-        // }
-        // constructor(props) {
-        //   super(props)
-        //   this.state = {      
-        //     counter: this.props.counter
-        //   }
-        // }
-        // IncroClick = () => {
-        //   if(this.state.counter<50){
-        //     this.setState(state=>({
-        //       counter: state.counter+1
-        //     }))
-        //   }
-          
-        // }
-        // DecroClick = () =>{
-        //   if(this.state.counter>-50){
-        //     this.setState(state=>({
-        //       counter: state.counter - 1 
-        //     }))
-          
-        //   }
-        // }
-        // RandomClick = () => {
-        //   const min = -50;
-        //   const max = 50;
-        //   const rand = min + Math.random() * (max - min);
-        //   this.setState({ counter: Math.round(rand) });
-        // }
-        // NullClick= () =>{
-        //   this.setState(state=>({
-        //     counter: this.props.counter
-        //   }))
-        // }
--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,32 +1,35 @@
 import {Component} from 'react';
 import './employees-add-form.css';
 
-class EmployeesAddForm extends Component{
-    constructor(props){
-        super(props)
-        this.state= {
+class EmployeesAddForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
             name: '',
             salary: ''
         }
     }
-    onValueChange = (e) =>{
+
+    onValueChange = (e) => {
         this.setState({
-           [e.target.name]: e.target.value
+            [e.target.name] : e.target.value
         })
     }
-    onSubmit=(e)=>{
+
+    onSubmit = (e) => {
         e.preventDefault();
-        if(this.state.name !=='' && this.state.salary!==''){
-            this.props.onAdd(this.state.name, this.state.salary);
-            this.setState({
-                name: '',
-                salary: ''
-            })  
-        }
-        
+        // Можно еще и сообщения добавлять, подсветку, атрибуты minlength и тд.
+        if (this.state.name.length < 3 || !this.state.salary) return;
+        this.props.onAdd(this.state.name, this.state.salary);
+        this.setState({
+            name: '',
+            salary: ''
+        })
     }
-    render(){
-        const {name,salary} = this.state
+
+    render() {
+        const {name, salary} = this.state;
+
         return (
             <div className="app-add-form">
                 <h3>Добавьте нового сотрудника</h3>
@@ -35,22 +38,24 @@ class EmployeesAddForm extends Component{
                     onSubmit = {this.onSubmit}>
                     <input type="text"
                         className="form-control new-post-label"
-                        placeholder="Как его зовут?" onChange={this.onValueChange} name="name" value={name}/>
+                        placeholder="Как его зовут?"
+                        name="name"
+                        value={name} 
+                        onChange={this.onValueChange}/>
                     <input type="number"
                         className="form-control new-post-label"
-                        placeholder="З/П в $?" onChange={this.onValueChange} name="salary" value={salary}/>
-
-                    <button 
-                        type="submit"
-                        className="btn btn-outline-light"
-                    >
-                        Добавить
-                    </button>
+                        placeholder="З/П в $?"
+                        name="salary"
+                        value={salary} 
+                        onChange={this.onValueChange}/>
+    
+                    <button type="submit"
+                        className="btn btn-outline-light">Добавить</button>
                 </form>
             </div>
         )
     }
-    
 }
+
 
 export default EmployeesAddForm;
